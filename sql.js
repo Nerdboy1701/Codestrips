24872c78cf310fe99d5f275627e4a71f5c91e912
Eddy Mercado <Nerdboy1701@outlook.com>
2021-05-08 13:08:45 -0400

    Implement Create Strip

--- sql.js
+++ sql.js
@@ -1,16 +1,16 @@
-const sqlite3 = require("sqlite3");
+const sqlite3 = require('sqlite3');
 
-const db = new sqlite3.Database("./db.sqlite");
+const db = new sqlite3.Database('./db.sqlite');
 
 db.serialize(() => {
 	db.run(`DROP TABLE If EXISTS Strip`);
-	db.run(`CREATE TABLE Strip (
-        id INT PRIMARY KEY,
+	db.run(`CREATE TABLE IF NOT EXISTS Strip(
+        id INTEGER PRIMARY KEY,
         head TEXT NOT NULL, 
         body TEXT NOT NULL, 
         background TEXT NOT NULL, 
         bubble_type TEXT NOT NULL, 
-        bubble_text TEXT NOT NULL DEFAULT ' ', 
-        caption TEXT NOT NULL DEFAULT ' '
-    )`);
+        bubble_text TEXT NOT NULL DEFAULT "", 
+        caption TEXT NOT NULL DEFAULT ""
+    );`);
 });
